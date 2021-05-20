6f4b09a0bffe2d9632e531bfff56d5e0b7cdafc2
cliffybar <crowellclifford@utexas.edu>
2021-05-20 03:16:55 -0500

    created single-post view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -51,7 +51,10 @@ router.get('/login', (req, res) => {
 router.get('/', (req, res) => {
     console.log(req.session);
 
-    // other logic...
+    res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+    });
 });
 
 router.post('/logout', (req, res) => {
@@ -63,6 +66,54 @@ router.post('/logout', (req, res) => {
     else {
         res.status(404).end();
     }
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
